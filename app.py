f358f2ec1aa8cdcbb5d16c0b5fff3874ead61f5b
Tony Liu <tl0226yn@gmail.com>
2023-09-12 16:35:45 -0700

    implements add functionality

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 import os
 import firebase_admin
 from firebase_admin import firestore
+from flask import Response
+from uuid import uuid4
+import datetime
 
 app = Flask(__name__)
 
@@ -20,6 +23,10 @@ def hello():
     return '<h1>Hello, World!</h1>'
 
 @app.route('/add_entry/', methods=['POST'])
+# Request parameters:
+# user: The unique user ID for the user who made the post
+# date: The date of the post, format must be (YYYY-MM-DD)
+# content: The post's content
 def add_entry():
     request_data = request.get_json()
 
@@ -29,14 +36,21 @@ def add_entry():
 
     if request_data:
         if 'user' in request_data:
-            user = request.data['user']
+            user = request_data['user']
         if 'date' in request_data:
-            date = request.data['date']
+            date = request_data['date']
         if 'content' in request_data:
-            content = request.data['content']
+            content = request_data['content']
     
-    #adds to database
-    directory = db.collection('posts').document(user).document(date)
-    directory.set({"content": content})
-
-    return None
+    try:
+        if user == None or date == None or content == None:
+            return Response(f"Upload failed, invalid fields", status=400)
+        #adds to database
+        directory = db.collection("Users").document(user).collection("posts").document(str(uuid4()))
+        #splits date
+        formattedDate = datetime.datetime(int(date.split('-')[0]), int(date.split('-')[1]), int(date.split('-')[2]))
+        directory.set({"date": formattedDate, "content": content})
+    except Exception as e:
+        return Response(f"Upload failed due to exception: {e}", status=500)
+    
+    return Response("Upload successful", status=200)
